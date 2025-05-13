5a14090e4ec3852127f96598abad8004ba3a6310
Bernardo Vicentini <bernardovicentini99@gmail.com>
2025-05-13 20:46:17 -0300

    Add files via upload

---
+++ aula-19/script.js
@@ -0,0 +1,6 @@
+// document.writeln("Olá, meu codigo JavaScript! Usando o Documento Writeln");
+// // window.alert("Olá, meu codigo JavaScript! Usando o Documento Writeln")
+// // document.getElementById("resposta").innerHTML =
+// // window.confirm("Você está gostando do curso?");
+// document.getElementById("resposta").innerHTML = "O meu nome é " +
+// window.prompt("Qual é o seu nome?");
---
+++ aula-19/script2.js
@@ -0,0 +1,6 @@
+function enviaDados() 
+{
+
+window.alert ("O nome digitado foi: " + document.getElementById ("function").value);
+
+}
